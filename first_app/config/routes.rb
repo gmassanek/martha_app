cb9957a36ff56b663866e18f4c40f3ebabf0848d
massgd5 <geoff.massanek@gmail.com>
2011-02-12 14:21:05 -0500

    CSS and Links

--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -1,4 +1,6 @@
 FirstApp::Application.routes.draw do
+  resources :microposts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- martha_app/app/helpers/application_helper.rb
+++ martha_app/app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def title
+    base_title = "Acute Care Leadership Summit"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  def menu_link_to(name, options = {}, html_options = {})
+    class_info = html_options[:class]
+    class_info = "#{class_info} current"
+    html_options.merge!({ :class => class_info }) if current_page?(options)
+    link_to name, options, html_options
+  end 
 end
+
+
