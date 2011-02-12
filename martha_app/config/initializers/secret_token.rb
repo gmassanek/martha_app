ce05afa56939d97ce004110b728c0298384d3564
massgd5 <geoff.massanek@gmail.com>
2011-02-12 13:05:22 -0500

    Initial setup

---
+++ martha_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarthaApp::Application.config.secret_token = 'bb4bf06c158a93a2b14a439425c4fac340cec52b1805746376abb46fdcc8b22600dc175c39737d28dd8f47742e2181b4f3eae4687dcdd8515c8582ba676d01ca'
