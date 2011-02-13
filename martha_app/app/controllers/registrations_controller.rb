e2faaa4c3c47317bda7986a15f6a8e0b4c3f7d22
massgd5 <geoff.massanek@gmail.com>
2011-02-13 15:05:08 -0500

    Forms setup the show function

--- martha_app/app/controllers/registrations_controller.rb
+++ martha_app/app/controllers/registrations_controller.rb
@@ -1,14 +1,25 @@
 class RegistrationsController < ApplicationController
   def new
+    @registration = Registration.new
+    @title = "Sign Up"
   end
 
   def edit
   end
 
   def create
+    @registration = Registration.new(params[:user])
+    if @registration.save
+      # Handle a successful save.
+    else
+      @title = "Sign up"
+      render :action=>'new'
+    end
   end
 
   def show
+    @registration = Registration.find(params[:id])
+    @title = @registration.name
   end
 
 end
--- martha_app/app/models/registration.rb
+++ martha_app/app/models/registration.rb
@@ -11,11 +11,15 @@
 #
 
 class Registration < ActiveRecord::Base
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   attr_accessible :name, :email
 
   validates :name,
       :presence => true,
-      :length => { :within => 2..40 }
-  
+      :length => { :within => 4..40 }
+  validates :email,
+      :presence => true,
+      :format => {:with => email_regex}
 end
   
