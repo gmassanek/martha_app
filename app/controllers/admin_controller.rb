1ab1ac7a49ed433f3f0993364b673fc5a8163b4f
massgd5 <geoff.massanek@gmail.com>
2011-02-20 22:10:57 -0500

    Front Image, Hotfix for Payment Dates

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,16 +4,22 @@ class AdminController < ApplicationController
   end
 
   def registrations
-    @registrations = Registration.all
+    #@registrations = Registration.all
+    @registrations = Registration.find(:all, :order => :created_at)
   end
 
   def confirm_payment
     puts "========== About to confirm_payment"
     registration = Registration.find(params[:registration])
-    registration.paid_date = Time.now
+    #registration.paid_date = Time.now
     puts registration.paid_date
-    registration.save
-    flash[:message] = "#{registration.name}'s payment received."
+    #if registration.save then
+    #if registration.update_attributes(:paid_date => Time.now) then
+    if registration.update_attribute(:paid_date,Time.now) then
+      flash[:message] = "#{registration.name}'s payment received."
+    else
+      flash[:message] = "#{registration.name}'s payment could not be updated because the registration is invalid."
+    end
     redirect_to admin_registrations_path
   end
 
