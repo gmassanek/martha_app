682e5da1589439d84393478a7e3f1168c6678bd9
Geoff Massanek <gmassanek@gmail.com>
2011-06-30 20:48:52 -0400

    would have emailed martha twice.  I need tests in this app

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,20 +3,19 @@ class ContactMailer < ActionMailer::Base
   sendgrid_category :use_subject_lines
   sendgrid_enable   :ganalytics, :opentracking
   default :from => "Acute Care Leadership<contact@acutecareleadershipsummit.com>"
-  default :bcc => ["mkenned2@jhmi.edu"]
+  defalt:to => 'phood2@jhmi.edu'
+  default :cc => ["mkenned2@jhmi.edu"]
 
   def message_from_user(contact)
     sendgrid_category "Acute Care Leadership Summit - User Comment"
     @contact = contact
-    mail(:to => 'phood2@jhmi.edu',
-         :subject => "Acute Care Leadership Summit - User Comment",
+    mail(:subject => "Acute Care Leadership Summit - User Comment",
          :from => "#{@contact.name}<#{@contact.email}>")
   end
   def questionaire_completed(questionaire)
     sendgrid_category "Acute Care Leadership Summit - Questionaire Completed"
     @questionaire = questionaire
-    mail(:to => 'mkenned2@jhmi.edu',
-         :subject => "Acute Care Leadership Summit - Questionaire Completed")
+    mail(:subject => "Acute Care Leadership Summit - Questionaire Completed")
   end
 end
 
