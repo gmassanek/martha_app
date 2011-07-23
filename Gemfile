2d12ae9f0da590a20728b4cf5f3724a086a0d6e5
Geoff Massanek <gmassanek@gmail.com>
2011-07-23 16:24:33 -0400

    New flier, updated line on the homepage

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rails', '3.0.3'
 
 #gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'pg'
-gem 'annotate-models', '1.0.4'
+#gem 'annotate-models'#, '1.0.4'
 gem 'factory_girl_rails', '1.0'
 gem 'sendgrid'
 gem 'jrails'
--- db/schema.rb
+++ db/schema.rb
@@ -62,13 +62,6 @@ ActiveRecord::Schema.define(:version => 20110608014121) do
     t.string   "last_name"
   end
 
-  create_table "subscriptions", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", :force => true do |t|
     t.string   "name"
     t.text     "salt"
